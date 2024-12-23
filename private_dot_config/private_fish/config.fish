d557c62efcb9e9363e8015845b5f34db2b9f8d00
dnx04 <86938955+dnx04@users.noreply.github.com>
2024-12-23 23:47:11 +0700

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,17 +4,4 @@ end
 
 fzf_configure_bindings --directory=\ed --git_status=\eg --git_log=\el --history=\eh --variables=\ev --processes=\ep
 zoxide init fish | source
-atuin init fish | source
 set -gx GPG_TTY (tty)
-
-fish_add_path -a /home/dnx04/.foundry/bin
-
-# Created by `pipx` on 2024-08-10 02:48:40
-set PATH $PATH /home/dnx04/.local/bin
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-set -gx GEM_HOME "$(gem env user_gemhome)"
-set -gx PATH $PATH $GEM_HOME/bin
